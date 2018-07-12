34ad9811f8715d50eafc74158f25a1b4a9303e66
Krystian Kozłowski <34834953+krystek97@users.noreply.github.com>
2018-07-12 16:31:11 +0200

    RangeVariable
    v 1.0

---
+++ RangeVariable/src/rangevariable/sportcode/pl/GlobalVariable.java
@@ -0,0 +1,11 @@
+package rangevariable.sportcode.pl;
+
+public class GlobalVariable 
+{
+   int globalvariable = 5 ;
+   
+   public static void main(String[] args)
+   {
+	   
+   }
+}
---
+++ RangeVariable/src/rangevariable/sportcode/pl/LocalVariable.java
@@ -0,0 +1,14 @@
+package rangevariable.sportcode.pl;
+
+public class LocalVariable 
+{
+	public static void main(String[] args)
+	{
+		int number = 0 ;
+		while(number<3)
+		{
+			number ++ ;
+			int localvariable ;
+		}
+	}
+}
